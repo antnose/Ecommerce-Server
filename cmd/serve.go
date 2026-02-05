55d44366e4f2feb31fd3d7395abafb569ee76eec
antnose <antnose28@gmail.com>
2026-02-05 16:54:48 +0600

    Separete every single file

--- cmd/serve.go
+++ cmd/serve.go
@@ -2,33 +2,11 @@ package cmd
 
 import (
 	"ecommerce/config"
-	"ecommerce/middleware"
-	"fmt"
-	"net/http"
-	"os"
-	"strconv"
+	"ecommerce/rest"
 )
 
 func Serve() {
 	cnf := config.GetConfig()
 
-	manager := middleware.NewManager()
-	manager.Use(
-		middleware.Preflight,
-		middleware.Cors,
-		middleware.Logger,
-	)
-
-	mux := http.NewServeMux()
-	wrappedMux := manager.WrapMux(mux)
-
-	initRoutes(mux, manager)
-
-	fmt.Println("Server is running on port: 3000")
-	addr := ":" + strconv.Itoa(cnf.HttpPort)
-	err := http.ListenAndServe(addr, mux)
-	if err != nil {
-		fmt.Println("Error starting server", wrappedMux)
-		os.Exit(1)
-	}
+	rest.Start(cnf)
 }
